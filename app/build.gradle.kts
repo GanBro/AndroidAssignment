42c2c0bce75400008cef64de44e7852278176435
wubo <2551921037@qq.com>
2024-06-21 22:12:45 +0800

    个人中心ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     kapt("androidx.room:room-compiler:2.5.1")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     kapt("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/ganbro/shopmaster/fragments/ProfileFragment.java
+++ app/src/main/java/com/ganbro/shopmaster/fragments/ProfileFragment.java
@@ -4,17 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import com.ganbro.shopmaster.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        CircleImageView profileImage = view.findViewById(R.id.profile_image);
+        View loginRegisterButton = view.findViewById(R.id.login_register_button);
+
+        // 设置点击事件
+        loginRegisterButton.setOnClickListener(v -> {
+            // 处理登录/注册按钮点击事件
+        });
+
+        // 根据实际需要进行更多设置
     }
 }
