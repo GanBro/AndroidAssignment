ac57abdb36254f1882981b90033c6a2dbfacebac
wubo <2551921037@qq.com>
2024-06-26 10:50:18 +0800

    视频收藏

--- app/src/main/java/com/ganbro/shopmaster/activities/FavoritesActivity.java
+++ app/src/main/java/com/ganbro/shopmaster/activities/FavoritesActivity.java
@@ -1,55 +1,26 @@
 package com.ganbro.shopmaster.activities;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import com.ganbro.shopmaster.R;
-import com.ganbro.shopmaster.adapters.ProductAdapter;
-import com.ganbro.shopmaster.adapters.VideoAdapter;
-import com.ganbro.shopmaster.database.ProductDao;
-import com.ganbro.shopmaster.database.VideoDatabaseHelper;
-import com.ganbro.shopmaster.models.Product;
-import com.ganbro.shopmaster.models.Video;
-import java.util.List;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class FavoritesActivity extends AppCompatActivity {
-
-    private RecyclerView recyclerViewProductFavorites;
-    private RecyclerView recyclerViewVideoFavorites;
-    private ProductAdapter productAdapter;
-    private VideoAdapter videoAdapter;
-    private ProductDao productDao;
-    private VideoDatabaseHelper videoDatabaseHelper;
-    private int userId;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
 
-        recyclerViewProductFavorites = findViewById(R.id.recycler_view_product_favorites);
-        recyclerViewVideoFavorites = findViewById(R.id.recycler_view_video_favorites);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager2 viewPager = findViewById(R.id.view_pager);
 
-        recyclerViewProductFavorites.setLayoutManager(new LinearLayoutManager(this));
-        recyclerViewVideoFavorites.setLayoutManager(new LinearLayoutManager(this));
+        viewPager.setAdapter(new com.ganbro.shopmaster.activities.ViewPagerAdapter(this));
 
-        productDao = new ProductDao(this);
-        videoDatabaseHelper = new VideoDatabaseHelper(this);
-
-        // 获取 SharedPreferences 中保存的用户ID
-        SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
-        userId = sharedPreferences.getInt("user_id", -1);
-
-        List<Product> favoriteProducts = productDao.getAllFavorites(userId);
-        List<Video> favoriteVideos = videoDatabaseHelper.getAllFavoriteVideos();
-
-        productAdapter = new ProductAdapter(this, favoriteProducts, false);
-        videoAdapter = new VideoAdapter(this, favoriteVideos);
-
-        recyclerViewProductFavorites.setAdapter(productAdapter);
-        recyclerViewVideoFavorites.setAdapter(videoAdapter);
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            tab.setText(position == 0 ? "商品收藏" : "视频收藏");
+        }).attach();
     }
 }
--- app/src/main/java/com/ganbro/shopmaster/fragments/FavoritesFragment.java
+++ app/src/main/java/com/ganbro/shopmaster/fragments/FavoritesFragment.java
@@ -1,57 +1,72 @@
-package com.ganbro.shopmaster.fragments;
+package com.ganbro.shopmaster.activities;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import com.ganbro.shopmaster.R;
 import com.ganbro.shopmaster.adapters.ProductAdapter;
+import com.ganbro.shopmaster.adapters.VideoAdapter;
 import com.ganbro.shopmaster.database.ProductDao;
+import com.ganbro.shopmaster.database.VideoDatabaseHelper;
 import com.ganbro.shopmaster.models.Product;
+import com.ganbro.shopmaster.models.Video;
 import java.util.List;
 
 public class FavoritesFragment extends Fragment {
 
-    private RecyclerView recyclerViewFavorites;
-    private ProductAdapter productAdapter;
+    private static final String ARG_TYPE = "type";
+    private String type;
+    private RecyclerView recyclerView;
+    private TextView title;
     private ProductDao productDao;
-    private TextView categoryAll;
-    private int userId;
+    private VideoDatabaseHelper videoDatabaseHelper;
+
+    public static FavoritesFragment newInstance(String type) {
+        FavoritesFragment fragment = new FavoritesFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TYPE, type);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_favorites, container, false);
 
-        recyclerViewFavorites = view.findViewById(R.id.recycler_view_favorites);
-        recyclerViewFavorites.setLayoutManager(new LinearLayoutManager(getContext()));
+        title = view.findViewById(R.id.title);
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        productDao = new ProductDao(getActivity());
+        if (getArguments() != null) {
+            type = getArguments().getString(ARG_TYPE);
+        }
 
-        // 获取 SharedPreferences 中保存的用户ID
-        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserPrefs", getActivity().MODE_PRIVATE);
-        userId = sharedPreferences.getInt("user_id", -1);
+        productDao = new ProductDao(getContext());
+        videoDatabaseHelper = new VideoDatabaseHelper(getContext());
 
-        List<Product> favoriteProducts = productDao.getAllFavorites(userId);
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("UserPrefs", getContext().MODE_PRIVATE);
+        int userId = sharedPreferences.getInt("user_id", -1);
 
-        productAdapter = new ProductAdapter(getActivity(), favoriteProducts, false);
-        recyclerViewFavorites.setAdapter(productAdapter);
-
-        categoryAll = view.findViewById(R.id.category_all);
-        categoryAll.setOnClickListener(v -> {
-            List<Product> allFavorites = productDao.getAllFavorites(userId);
-            productAdapter.setProductList(allFavorites);
-        });
-
-        // 其他类别按钮的设置...
+        if ("product".equals(type)) {
+            title.setText("商品收藏");
+            List<Product> favoriteProducts = productDao.getAllFavorites(userId);
+            ProductAdapter productAdapter = new ProductAdapter(getContext(), favoriteProducts, false);
+            recyclerView.setAdapter(productAdapter);
+        } else if ("video".equals(type)) {
+            title.setText("视频收藏");
+            List<Video> favoriteVideos = videoDatabaseHelper.getAllFavoriteVideos();
+            VideoAdapter videoAdapter = new VideoAdapter(getContext(), favoriteVideos);
+            recyclerView.setAdapter(videoAdapter);
+        }
 
         return view;
     }
